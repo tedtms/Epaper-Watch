84c06e358bd9febde76c3a8707e5a13fc633e3e4
Ted Sadler <ted@sadlers.io>
2023-09-19 22:18:04 -0400

    Initial push to github

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@
 #include <soc/soc.h>
 #include <soc/rtc_cntl_reg.h>
 
-#include "secrets.h" //be sure to create this file within the src/ directory and populate the following variables:
+#include "secrets.h" //be sure to create this file within the src/ directory and populate the following lines:
 //const char *ssid = "";      //WiFi SSID;
 //const char *password = "";  //WiFi SSID passkey;
 //const char *host = "";      //Weather API host
